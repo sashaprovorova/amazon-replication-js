00e3915c27d8fe80eea2317de7510bd4ededc151
Aleksandra Provorova <sashaprovora@gmail.com>
2024-12-22 12:32:50 -0500

    make add button interactive

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -47,14 +47,38 @@ products.forEach((product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
   `;
 } );
 
-console.log(productsHTML);
-
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', ()=>{
+    const productId =button.dataset.productId;
+
+    let mathingItem;
+    cart.forEach((item) => {
+      if(productId === item.productId){
+        mathingItem = item;
+      }
+    });
+
+    if(mathingItem) {
+      mathingItem.quantity+=1;
+    }
+    else {
+      cart.push(
+        {
+          productId: productId, 
+          quantity: 1
+        }
+       );
+    }
+    console.log(cart);
+  });
+});
