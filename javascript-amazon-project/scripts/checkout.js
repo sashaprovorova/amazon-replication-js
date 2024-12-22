54ca1601335d93456dc1c08faacb4ba32e51e74d
Aleksandra Provorova <sashaprovora@gmail.com>
2024-12-22 17:26:33 -0500

    delete products from screen

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
 
   
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -102,7 +102,10 @@ document.querySelectorAll('.js-delete-quantity-link')
   .forEach((link) => {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
-      console.log(productId);
       removeFromCart(productId);
+      const container = document.querySelector(`.js-cart-item-container-${productId}`); 
+      container.remove();
+      // console.log(container);
     });
-  });
+});
+
