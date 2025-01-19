c1ca15beb897eb5007dfbb693a5095e81222e342
Aleksandra Provorova <sashaprovora@gmail.com>
2025-01-19 18:05:57 -0500

    convert products into classes

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,6 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import '../data/cart-class.js'; //just runs the entire code from this file instead of importing stuff from it 
+// import '../data/cart-class.js'; //just runs the entire code from this file instead of importing stuff from it 
 
 renderOrderSummary();
 renderPaymentSummary();
