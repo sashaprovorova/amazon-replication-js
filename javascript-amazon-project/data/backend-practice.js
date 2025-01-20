cf7517961c1bb31733e64a892152883af6e855b9
Aleksandra Provorova <sashaprovora@gmail.com>
2025-01-20 17:59:25 -0500

    add backend practice file

---
+++ javascript-amazon-project/data/backend-practice.js
@@ -0,0 +1,16 @@
+//creates the message to backend
+const xhr = new XMLHttpRequest(); 
+
+//waits for the even before execusting 
+xhr.addEventListener('load', () => {
+  console.log(xhr.response);
+});
+
+//set up the request, include type of message and url
+xhr.open('GET', 'https://supersimplebackend.dev');
+
+//send the message
+xhr.send();
+
+//gets a response, undefined at first and need to wait for response to come back, so asynchornous
+//xhr.response //problematic, so use event listener
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../data/cart-class.js'; //just runs the entire code from this file instead of importing stuff from it 
+import '../data/backend-practice.js'
 
 renderOrderSummary();
 renderPaymentSummary();
