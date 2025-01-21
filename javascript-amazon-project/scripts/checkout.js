320e696dc5fbad4e2854f47acc7a8098fd6d5c6e
Aleksandra Provorova <sashaprovora@gmail.com>
2025-01-20 19:49:18 -0500

    learn promises and convert code to promises

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -3,8 +3,53 @@ import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../data/cart-class.js'; //just runs the entire code from this file instead of importing stuff from it 
 // import '../data/backend-practice.js'
 import {loadProducts} from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-loadProducts(() => {
+// runs and waits for multiple promises 
+Promise.all([
+  new Promise((resolve) => {
+
+    loadProducts(()=>{
+      resolve('value1'); //shares values between two steps
+    });
+  }), 
+  new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  })
+
+]).then((values)=>{
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+//resolve similar to done, lets us control when to go to the next step
+new Promise((resolve) => {
+  loadProducts(()=>{
+    resolve('value1'); //shares values between two steps
+  });
+}).then((value)=>{
+  console.log(value); //gets the value from previous step
+  return new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  }).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+})
+*/
+
+
+/*
+loadProducts(() => {
+  loadCart(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
