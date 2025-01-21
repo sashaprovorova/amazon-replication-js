98a14af90bc2167faa654627360b51f9c1b55f1d
Aleksandra Provorova <sashaprovora@gmail.com>
2025-01-21 13:33:04 -0500

    replace xhr th fetch

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -2,17 +2,18 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../data/cart-class.js'; //just runs the entire code from this file instead of importing stuff from it 
 // import '../data/backend-practice.js'
-import {loadProducts} from '../data/products.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
 import { loadCart } from '../data/cart.js';
 
 // runs and waits for multiple promises 
 Promise.all([
+  loadProductsFetch(), //replaces the following
+  /*
   new Promise((resolve) => {
-
     loadProducts(()=>{
       resolve('value1'); //shares values between two steps
     });
-  }), 
+  }), */
   new Promise((resolve)=>{
     loadCart(()=>{
       resolve();
--- javascript-amazon-project/tests/checkout/orderSummaryTest.js
+++ javascript-amazon-project/tests/checkout/orderSummaryTest.js
@@ -1,6 +1,6 @@
 import  {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage, cart} from '../../data/cart.js';
-import { loadProducts } from '../../data/products.js';
+import {loadProducts, loadProductsFetch} from '../../data/products.js';
 
 describe('test suite: renderOrderSummary', () => {
 
@@ -10,7 +10,7 @@ describe('test suite: renderOrderSummary', () => {
   // hook to run once before all tests
   // done prevents going to the next step without waiting
   beforeAll((done) => {
-    loadProducts(() => {
+    loadProductsFetch().then(()=>{
       done();
     });
   });
