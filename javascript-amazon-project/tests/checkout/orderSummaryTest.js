a02997946f9ab4d8696b661ea1f10e4fd27578b8
Aleksandra Provorova <sashaprovora@gmail.com>
2025-01-20 19:08:01 -0500

    load all products in beforeall hook

--- javascript-amazon-project/tests/checkout/orderSummaryTest.js
+++ javascript-amazon-project/tests/checkout/orderSummaryTest.js
@@ -1,47 +1,55 @@
 import  {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage, cart} from '../../data/cart.js';
+import { loadProducts } from '../../data/products.js';
 
 describe('test suite: renderOrderSummary', () => {
 
-const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
-//hook for the tests that runs before each test
-beforeEach(()=> {
-  spyOn(localStorage, 'setItem');
-    document.querySelector('.js-test-container').innerHTML = `
-      <div class="js-order-summary"></div>
-      <div class="js-payment-summary"></div>
-    `;
-
-    spyOn(localStorage, 'getItem').and.callFake( () => {
-          return JSON.stringify([{
-            productId: productId1, 
-            quantity: 2, 
-            deliveryOptionId: '1'
-          }, 
-          {
-            productId: productId2, 
-            quantity: 1, 
-            deliveryOptionId: '2'
-          }
-          ]);
-  });
-  loadFromStorage();
-
-  renderOrderSummary();
+  // hook to run once before all tests
+  // done prevents going to the next step without waiting
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    });
   });
 
-  it('displays the cart', () => {
+  //hook for the tests that runs before each test
+  beforeEach(()=> {
+    spyOn(localStorage, 'setItem');
+      document.querySelector('.js-test-container').innerHTML = `
+        <div class="js-order-summary"></div>
+        <div class="js-payment-summary"></div>
+      `;
 
-    expect (
-      document.querySelectorAll('.js-cart-item-container').length).toEqual(2);
-    expect (
-      document.querySelector(`.js-product-quantity-${productId1}`).innerText).toContain('Quantity: 2');
+      spyOn(localStorage, 'getItem').and.callFake( () => {
+            return JSON.stringify([{
+              productId: productId1, 
+              quantity: 2, 
+              deliveryOptionId: '1'
+            }, 
+            {
+              productId: productId2, 
+              quantity: 1, 
+              deliveryOptionId: '2'
+            }
+            ]);
+    });
+    loadFromStorage();
+
+    renderOrderSummary();
+    });
+
+    it('displays the cart', () => {
+
       expect (
-        document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1');
+        document.querySelectorAll('.js-cart-item-container').length).toEqual(2);
+      expect (
+        document.querySelector(`.js-product-quantity-${productId1}`).innerText).toContain('Quantity: 2');
+        expect (
+          document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1');
   });
-
 
   it('removes a product', () => {
 
